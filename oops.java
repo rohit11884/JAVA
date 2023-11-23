ccd0807819fb88d884d62f3f691c27a4ea916668
Rohit <rohit11884kr@gmail.com>
2023-11-23 14:46:33 +0530

    just checking

--- oops.java
+++ oops.java
@@ -1,14 +1,19 @@
 class Employee{
     int id;
     String name;
+    public void printdetail(){
+        System.out.println("My ID is "+id);
+        System.out.println("My Name is "+ name);
+    }
+   
 }
 public class oops {
-    public static void main(String[] args) {
-        Employee em=new Employee();
-        em.id=12;
-        em.name="Rohit";
-        System.out.println(em.id);
-        System.out.println(em.name);
+     public static void main(String[] args) {
+        Employee em1=new Employee();
+        Employee em2=new Employee();
+        em1.id=12;
+        em1.name="Rohit";
+        em1.printdetail();
         
     }
 }
