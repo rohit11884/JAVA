e13df75ca6619cd71a3875d1588a5d008859e247
Rohit <rohit11884kr@gmail.com>
2023-11-23 15:49:34 +0530

    solving problem

--- oops.java
+++ oops.java
@@ -1,9 +1,11 @@
 class Employee{
     int id;
     String name;
+    int salary;
     public void printdetail(){
-        System.out.println("My ID is "+id);
         System.out.println("My Name is "+ name);
+        System.out.println("My ID is "+id);
+        System.out.println("My salary is "+salary);
     }
    
 }
@@ -11,9 +13,15 @@ public class oops {
      public static void main(String[] args) {
         Employee em1=new Employee();
         Employee em2=new Employee();
-        em1.id=12;
         em1.name="Rohit";
+        em1.id=12;
+        em2.name="Akash";
+        em2.id=14;
+        em1.salary=2000000;
+        em2.salary=3000000;
         em1.printdetail();
+        em2.printdetail();
+       
         
     }
 }
---
+++ practiceset.java
@@ -0,0 +1,39 @@
+class Employee{
+    int salary;
+    String name;
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+
+
+   
+    
+}
+public class practiceset {
+    public static void main(String[] args) {
+        Employee em =new Employee();
+        em.setName("Rohit");
+        em.getName();
+        System.out.println(em.getName());
+        em.setSalary(123444555);
+        em.getSalary();
+        System.out.println(em.getSalary());
+    }
+    
+    
+
+}
